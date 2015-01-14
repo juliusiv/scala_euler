0d726f1120919e437f81b5e20b424479c4aeb53e
Julius Alexander IV <alexaj5rpi@gmail.com>
2015-01-13 23:14:39 -0500

    Finished Four.

--- Ten/Ten.scala
+++ Ten/Ten.scala
@@ -19,8 +19,8 @@ object Ten {
   def ArgMatch(arg: String): Any = arg match {
     case "1" => One
     case "2" => Two
-    case "3" => "Three"
-    case "4" => "Four"
+    case "3" => Three
+    case "4" => Four
     case "5" => "Five"
     case "6" => "Six"
     case "7" => "Seven"
@@ -31,6 +31,9 @@ object Ten {
   }
 
 
+  /*
+    Find the sum of all the multiples of 3 or 5 below 1000.
+  */
   def One(): Long = {
     val ceil = 1000
 
@@ -42,6 +45,8 @@ object Ten {
       else Mult3or5_rec(n+1)
     }
     
+    // Uses a comprehension to get all the multiples of 3 or 5 and then sums
+    // them.
     def Mult3or5_comp(n: Long): List[Long] = {
       for(i <- List.range(0, n) if i%3 == 0 || i%5 == 0) yield i
     }
@@ -50,15 +55,27 @@ object Ten {
   }
 
 
+  /*
+    Find the sum of the even-valued terms of the Fibonacci sequence whose values
+    do not exceed four million.
+  */
   def Two(): Long = {
     val ceil = 4000000
     var sum: Long = 2
 
+    // Uses a Stream to create the Fibonacci sequence and then accumulates all
+    // the even values.
     def FibSum_lazy(): Long = {
-      lazy val fib: Stream[Int] = 0 #:: 1 #:: fib.zip(fib.tail).map {n => n._1 + n._2}
-      fib.takeWhile {x => x <= ceil} .foldLeft(0) {(acc, x) => if (x%2 == 0) acc+x else 0+acc}
+      lazy val fib: Stream[Int] = 0 #:: 1 #:: fib.zip(fib.tail).map {
+        n => n._1 + n._2
+      }
+      fib.takeWhile {x => x <= ceil} .foldLeft(0) {
+        (acc, x) => if (x%2 == 0) acc+x else 0+acc
+      }
     }
 
+    // Recursively finds the next term of the Fibonacci sequence and adds them
+    // up as they go.
     def FibSum_rec(last_2: Int, last_1: Int): Long = { 
       if (last_2+last_1 > ceil) sum
       else {
@@ -71,9 +88,33 @@ object Ten {
   }
 
 
-  // def Three(): Int = {
-  //   def PrimeFactors(n: Long): List[Int] = {
+  /*
+    What is the largest prime factor of the number 600851475143?
+  */
+  def Three(): Long = {
+    val ints = Stream.from(2)
+    val sieve = ints.head #:: ints.tail.filter {x => x%ints.head != 0}
 
-  //   }
-  // }
+    // Relies on the fact that once we've found a prime that is not a factor,
+    // the remaining prime factors must be larger than the last prime factors.
+    def PrimeFactors(n: Long, primes: Stream[Int]): Long = {
+      if (n == primes.head) n
+      else if (n%primes.head == 0) PrimeFactors(n/primes.head, primes)
+      else PrimeFactors(n, primes.tail)
+    }
+    PrimeFactors(600851475143L, sieve)
+  }
+
+
+  /*
+    Find the largest palindrome made from the product of two 3-digit numbers.
+  */
+  def Four(): Int = {
+    val ints: List[Int] = List.range(1, 1000)
+    val mults = for ( i <- ints) yield ints.map {x => i*x}
+    val palindromes = mults.flatten.filter {x => x.toString == x.toString.reverse}
+    palindromes.max
+  }
+
+
 }
